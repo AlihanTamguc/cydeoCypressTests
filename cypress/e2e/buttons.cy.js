fa13a5df6b01b65290974c02b1f2f8870d677921
AlihanTamguc <alihanwit@gmail.com>
2023-08-15 13:39:54 +0200

    buttons file added

---
+++ cypress/e2e/buttons.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+
+
+describe('Context : My First Tests', () => {
+
+
+    beforeEach(() => {
+
+
+ 
+        cy.clearAllCookies();
+        cy.visit('/multiple_buttons');
+    })
+
+
+
+
+
+    it('Check different button actions', () => {
+
+
+
+
+
+
+
+
+
+    })
+})
+
+

fa181a2607690a01e0d404177895397f6b0504a0
AlihanTamguc <alihanwit@gmail.com>
2023-08-15 14:14:45 +0200

    buttons updated

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -23,6 +23,24 @@ describe('Context : My First Tests', () => {
 cy.contains('Button 2').should('be.visible').click();
 cy.contains('Clicked on button two!').should('be.visible');
 
+// find element with class attribute and create a list then select 3rd element
+
+// by using wrap method, I am turning JQuery element into Cypress Element
+
+cy.get('.btn.btn-primary').then(($buttons) => {
+
+    cy.wrap($buttons).eq(2).click();
+
+   // assert the text 
+
+   cy.contains('Clicked on button three!').should('be.visible');
+
+    })
+
+
+
+
+
 
 
 
